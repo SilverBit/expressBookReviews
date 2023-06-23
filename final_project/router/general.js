5f458ff5acea68464dc5b252a1b309df9c87f8fe
Kleomenis Rousias <csd4099@csd.uoc.gr>
2023-06-23 13:24:24 +0300

    Until Task 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,60 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+  const isbn = req.params.isbn;
+
+   res.send(books[isbn]);
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookKeys = Object.keys(books);
+  const author_books = [];
+
+  bookKeys.forEach((key) => {
+  const book = books[key];
+  if (book.author === author) {
+      author_books.push(book);
+  }
+  });
+
+  res.send(JSON.stringify(author_books));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookKeys = Object.keys(books);
+  const title = req.params.title;
+
+  bookKeys.forEach((key) => {
+  const book = books[key];
+  if (book.title === title) {
+      res.send(JSON.stringify(book));
+  }
+  });
+  res.send("Book Not Found!");
 });
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const bookKeys = Object.keys(books);
+
+  bookKeys.forEach((key) => {
+      const book = books[key];
+      console.log(key);
+      if (key === isbn) {
+          
+          res.send(JSON.stringify(book.reviews));
+      }
+      });
 });
 
 module.exports.general = public_users;
