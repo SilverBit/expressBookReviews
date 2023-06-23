17f9df9120c74285dd0ab8927408094722de277a
Kleomenis Rousias <csd4099@csd.uoc.gr>
2023-06-23 16:57:15 +0300

    New Task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,8 +11,15 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+  let validusers = users.filter((user)=>{
+    return (user.username === username && user.password === password)
+  });
+  if(validusers.length > 0){
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
@@ -22,17 +29,21 @@ regd_users.post("/login", (req,res) => {
   const passwordLog = req.body.password;
 
   //Validate
-  const foundUser = users.find((user) => user.username === usernameLog && user.password === passwordLog);
-  console.log(users);
-  if (foundUser) {
-      // Generate a JWT token
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+      return res.status(404).json({message: "Error logging in"});
+  }
+  //Generate Token
+  if (authenticatedUser(username,password)) {
       let accessToken = jwt.sign({ username: usernameLog },'access');
-  
+    
       return res.status(200).send("User successfully logged in");
-      } else {
-          return res.status(208).json({message: "Invalid Login. Check username and password"});
-      }
+  } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+  }
 });
+
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
