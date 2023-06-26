69a8e2d2c80b05b665dd9464eed16c31e65133d0
Kleomenis Rousias <csd4099@csd.uoc.gr>
2023-06-26 15:02:31 +0300

    Task 10

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -91,4 +91,15 @@ public_users.get('/review/:isbn',function (req, res) {
       });
 });
 
+
+//Axios Promise : Get book list
+
+public_users.get('/books',function (req, res) {
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+      get_books.then(() => console.log("Book list retrieved!"));
+  });
+
+  
 module.exports.general = public_users;
